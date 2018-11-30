2c424e2f29a3f75a0448c35175c716fbb227b2c3
amanation <fractalemanation@mail.ru>
2018-11-30 21:06:45 +0200

    update

--- web/index.php
+++ web/index.php
@@ -7,6 +7,8 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
+use FormulaParser\FormulaParser;
+
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
@@ -25,7 +27,16 @@ $app->post('/bot', function() use($app) {
 			return getenv('VK_CONFIRMATION_CODE');
 			break;
 		case 'message_new':
-			$request_params = array('user_id' => $data->object->user_id, 'message' => 'Тест', 'access_token' => getenv('VK_SECRET_TOKEN'), 'v' => '5.69');
+			$formula = '3*x^2 - 4*y + 3/y';
+			$precision = 2; 
+			$request_params = array('user_id' => $data->object->user_id, 'message' => 'Message text', 'access_token' => getenv('VK_SECRET_TOKEN'), 'v' => '5.69');
+			try {
+			    $parser = new FormulaParser($formula, $precision);
+			    $result = $parser->getResult();
+			    $request_params['message'] = 'Ответ: '.$result[1];
+			} catch (\Exception $e) {
+			    $request_params['message'] = 'Указана неверная формула, попробуйте снова!';
+			}
 			file_get_contents('https://api.vk.com/method/messages.send?'.http_build_query($request_params));
 			return 'ok';
 			break;
