ffd02b8ea387f491a162a40c619d8ed3eb91c8cb
amanation <fractalemanation@mail.ru>
2018-11-30 21:08:10 +0200

    update

--- web/index.php
+++ web/index.php
@@ -27,13 +27,11 @@ $app->post('/bot', function() use($app) {
 			return getenv('VK_CONFIRMATION_CODE');
 			break;
 		case 'message_new':
-			$formula = '3*x^2 - 4*y + 3/y';
-			$precision = 2; 
 			$request_params = array('user_id' => $data->object->user_id, 'message' => 'Message text', 'access_token' => getenv('VK_SECRET_TOKEN'), 'v' => '5.69');
 			try {
 			    $parser = new FormulaParser($formula, $precision);
 			    $result = $parser->getResult();
-			    $request_params['message'] = 'Ответ: '.$result[1];
+			    $request_params['message'] = 'Ответ: '.number_format($result[1], 2, '.', ',');
 			} catch (\Exception $e) {
 			    $request_params['message'] = 'Указана неверная формула, попробуйте снова!';
 			}
