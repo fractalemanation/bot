4983da492444a7a20c837e85863a00b101f65209
amanation <fractalemanation@mail.ru>
2018-11-30 20:28:40 +0200

    update

--- web/index.php
+++ web/index.php
@@ -25,7 +25,7 @@ $app->post('/bot', function() use($app) {
 			return getenv('VK_CONFIRMATION_CODE');
 			break;
 		case 'message_new':
-			$request_params = array('user_id' => $data->object->user_id, 'message' => 'Test', 'access_token' => getenv('VK_SECRET_CODE'), 'v' => '5.69');
+			$request_params = array('user_id' => $data->object->user_id, 'message' => 'Тест', 'access_token' => getenv('VK_SECRET_CODE'), 'v' => '5.69');
 			file_get_contents('https://api.vk.com/method/messages.send?'.http_build_query($request_params));
 			return 'ok';
 			break;
