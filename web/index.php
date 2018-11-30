dba2c7dac6daa774921497a7f0080983ecdaaa1a
amanation <fractalemanation@mail.ru>
2018-11-30 20:59:23 +0200

    fg

--- web/index.php
+++ web/index.php
@@ -19,13 +19,13 @@ $app->get('/', function() use($app) {
 $app->post('/bot', function() use($app) {
 	$data = json_decode(file_get_contents('php://input'));
 	if (!$data) return 'wastedFirst';
-	if ($data->secret !== getenv('VK_SECRET_TOKEN') && $data->type !== 'confirmation') return getenv('VK_SECRET_TOKEN').'}}}}}}}}'.$data->secret;
+	if ($data->secret !== getenv('VK_SECRET_CODE') && $data->type !== 'confirmation') return 'wastedSecond';
 	switch ($data->type) {
 		case 'confirmation':
 			return getenv('VK_CONFIRMATION_CODE');
 			break;
 		case 'message_new':
-			$request_params = array('user_id' => $data->object->user_id, 'message' => 'Тест', 'access_token' => getenv('VK_SECRET_CODE'), 'v' => '5.69');
+			$request_params = array('user_id' => $data->object->user_id, 'message' => 'Тест', 'access_token' => getenv('VK_SECRET_TOKEN'), 'v' => '5.69');
 			file_get_contents('https://api.vk.com/method/messages.send?'.http_build_query($request_params));
 			return 'ok';
 			break;
